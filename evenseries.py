38d74c784796d1e19112d766b685171f0746550e
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-03-15 17:57:13 +0530

    while

---
+++ evenseries.py
@@ -0,0 +1,8 @@
+i=int(input("Enter Start limit :"))#1
+n=int(input("Enter End Limit :"))#10
+while i<=n:
+    if(i%2!=0):#5%2=0
+        print(i,end=" ")#1 3 5
+    i=i+1#2 3 4 5
+print()
+print("Loop Ended")
---
+++ while2.py
@@ -0,0 +1,7 @@
+i=int(input("Enter start limit :"))#1
+n=int(input("Enter End limit :"))#10
+while i<=n:
+    print(i,end=" ")
+    i=i+2
+print()
+print("Loop ended")
