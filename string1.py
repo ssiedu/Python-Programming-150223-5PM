d13bc0989b2fe155cbdd608cfd061c9a4a486e81
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-03-20 19:56:44 +0530

    program

---
+++ string1.py
@@ -0,0 +1,13 @@
+str1='Hello World'
+str2="Hello Everyone"
+str3='''Welcome
+to
+ssi Digital'''
+str4="""This
+is a
+python class"""
+
+print("String 1:",str1)
+print("String 2:",str2)
+print("String 3:",str3)
+print("String 4:",str4)
